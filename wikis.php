0d81209a37c2599c92ecc38a5b40fcf812b605e4
chapatiyaq <chapatiyaq@gmail.com>
2017-02-01 17:40:57 +0100

    Adding alpha wikis: Battlerite, Diabotical, LoL, WoW, FIFA

---
+++ wikis.php
@@ -0,0 +1,26 @@
+<?php
+$default_wikis = array('starcraft', 'starcraft2', 'dota2', 'hearthstone', 'heroes', 'smash', 'counterstrike', 'overwatch', 'commons', 'warcraft', 'fighters', 'rocketleague');
+
+$wiki_names = array(
+	'starcraft' => 'StarCraft: Brood War',
+	'starcraft2' => 'StarCraft II',
+	'dota2' => 'Dota 2',
+	'hearthstone' => 'Hearthstone',
+	'heroes' => 'Heroes',
+	'smash' => 'Smash Bros',
+	'counterstrike' => 'Counter-Strike',
+	'overwatch' => 'Overwatch',
+	'commons' => 'Commons',
+	'warcraft' => 'Warcraft',
+	'fighters' => 'Fighting Games',
+	'rocketleague' => 'Rocket League',
+	'clashroyale' => 'Clash Royale',
+	'crossfire' => 'CrossFire',
+	'battlerite' => 'Battlerite',
+	'teamfortress' => 'Team Fortress',
+	'trackmania' => 'TrackMania',
+	'diabotical' => 'Diabotical',
+	'leagueoflegends' => 'League of Legends',
+	'worldofwarcraft' => 'World of Warcraft',
+	'fifa' => 'FIFA'
+);
