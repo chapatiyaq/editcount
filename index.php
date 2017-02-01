71638196b6cb54df150325352bf3a2199ef59e03
chapatiyaq <chapatiyaq@gmail.com>
2017-02-01 18:29:21 +0100

    Fixing the previous edit

--- index.php
+++ index.php
@@ -69,7 +69,7 @@ foreach ($get_wikis as $wiki) {
 		<div id="wikis">
 			<span><b>Select wikis: </b></span>
 <?php foreach ($wiki_names as $wiki => $wiki_name) { ?>
-			<input type="checkbox" <?php echo isset($clean_wikis_list['<?php echo $wiki; ?>']) ? 'checked="checked" ' : ''; ?>name="wikis[]" value="<?php echo $wiki; ?>" id="<?php echo $wiki; ?>"/>
+			<input type="checkbox" <?php echo isset($clean_wikis_list[$wiki]) ? 'checked="checked" ' : ''; ?>name="wikis[]" value="<?php echo $wiki; ?>" id="<?php echo $wiki; ?>"/>
 			<label class="wiki-button <?php echo $wiki; ?>" for="<?php echo $wiki; ?>" title="<?php echo $wiki_name; ?>"></label>
 <?php } ?>
 		</div>
